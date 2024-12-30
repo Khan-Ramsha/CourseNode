9b7395932bc51b12aa8e3841a27b2bc2b31c9f9e
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-12-31 02:02:50 +0530

    chore: nptel syllabus data scraped

---
+++ backend/data_scrape/scrape_program.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
+
+# Load the dataframe
+cse_data = pd.read_csv("cse-jan-apr-2025.csv")
+# cse_data = df[df['Discipline'].isin(['Computer Science and Engineering', 'Computer Science', 'Electrical Engineering'])]
+
+# Function to scrape data from a single URL
+def scrape_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    content = soup.find('div', class_='previewContent marginTop20')
+    return content.get_text(separator="\n") if content else ""
+
+# Scrape data from all URLs and store in a dictionary
+scraped_data = {}
+for index, row in cse_data.iterrows():
+    url = row['URL']
+    course_id = row['ID']
+    scraped_data[course_id] = scrape_url(url)
+
+# Save the scraped data to a JSON file
+with open('scraped_data.json', 'w') as json_file:
+    json.dump(scraped_data, json_file, indent=4)
