166aa9ab79c9764c04f0faa2168f798d25abd87f
Khan-Ramsha <khanramsha302020@gmail.com>
2025-01-01 22:29:30 +0530

    chore: minor changes

---
+++ backend/src/data_preprocessing/data_preprocessing.py
@@ -0,0 +1,13 @@
+from preprocessing import load_data, extracting_syllabus
+from nlp_task import clean_text
+import json
+
+data = load_data("backend/data_scrape/scraped_data.json")
+data = extracting_syllabus(data)
+for course in data:
+    course["text"] = clean_text(course["text"])
+with open("backend/data_scrape/cleaned_scraped.json", "w") as file:
+    json.dump(data, file)
+with open("backend/data_scrape/cleaned_scraped.json" , 'r') as file:
+  data = json.load(file)
+  print(data)
