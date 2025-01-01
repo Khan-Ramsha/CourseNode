39170c35fa0fd9b663ba0d385f8826dd99c899c2
Khan-Ramsha <khanramsha302020@gmail.com>
2025-01-02 00:58:13 +0530

    chore: removing duplicates from pdf stuff

--- backend/src/user_syllabus_processing/syllabus_preprocessing.py
+++ backend/src/user_syllabus_processing/syllabus_preprocessing.py
@@ -5,13 +5,13 @@ pdf_path = "backend/artifacts/syllabus.pdf"
 def preprocess_text(text):
     # Convert to lowercase
     text = text.lower()
-
-    # Parse the text using spaCy
     doc = nlp(text)
+    tokens = [token.lemma_ for token in doc if not token.is_stop and token.is_alpha]
+    # Remove duplicates while maintaining order
+    unique_tokens = list(dict.fromkeys(tokens)) 
 
-    cleaned_text = " ".join([token.lemma_ for token in doc if not token.is_stop and token.is_alpha])
+    return " ".join(unique_tokens)
 
-    return cleaned_text
 module_content = extract_module_content(pdf_path)
 content = preprocess_text(module_content)
 print(content)
